b4c526921291fa4900c73722da1ca480e8683f8b
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-06-05 18:26:38 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,48 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import streamlit
 import streamlit as st
+import folium
+import streamlit_folium
+from streamlit_folium import folium_static
 
-"""
-# Welcome to Streamlit!
+#Layout da página
+name = "Mapa da Desigualdade"
+st.set_page_config(layout="wide", page_title=name)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+#Opções do menu
+st.sidebar.markdown('# Mapa da Desigualdade')
+options = st.sidebar.selectbox("Selecione uma opção:", ("Paraná", "Núcleo Territorial Central"))
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.text('Fixed width text: st.text')
+st.markdown('_Markdown_') # see *
+st.caption('Balloons. Hundreds of them: st.caption') 
+st.latex(r''' e^{i\pi} + 1 = 0 ''')
+st.write('Most objects: st.write') # df, err, func, keras!
+st.write(['st', 'is <', 3]) # see *
+st.title('My title')
+st.header('My header')
+st.subheader('My sub: st.subheader')
+st.code('for i in range(8): foo()')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.button('Hit me')
+#st.download_button('On the dl', data)
+st.checkbox('Check me out')
+st.radio('Radio', [1,2,3])
+st.selectbox('Select', [1,2,3])
+st.multiselect('Multiselect', [1,2,3])
+st.slider('Slide me', min_value=0, max_value=10)
+st.select_slider('Slide to select', options=[1,'2'])
+st.text_input('Enter some text')
+st.number_input('Enter a number')
+st.text_area('Area for textual entry')
+st.date_input('Date input')
+st.time_input('Time entry')
+st.file_uploader('File uploader')
+st.camera_input("一二三,茄子!")
+st.color_picker('Pick a color')
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if options == "Paraná":
+  st.subheader("Mapa da Desigualdade do Paraná")
+  categoria = st.sidebar.selectbox("Selecione uma categoria:", ("Renda e Riqueza", "Categoria 2", "Categoria 3", "Categoria 4"))
+  st.sidebar.markdown("**Dica:** Feche este menu para uma melhor visualização dos mapas.")
+  if categoria == "Renda e Riqueza":
+    st.header("Renda e Riqueza do Paraná")
