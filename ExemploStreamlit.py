eb68bd8403796f3122138bd1b042cef14925b890
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-06-16 14:39:05 -0300

    Update ExemploStreamlit.py

--- ExemploStreamlit.py
+++ ExemploStreamlit.py
@@ -29,26 +29,26 @@ if options == "Paraná":
       st.header('My header')
       st.subheader('My sub: st.subheader')
       st.code('for i in range(8): foo()')
-    elif categoria == "Categoria 2":
-      st.button('Hit me')
-      #st.download_button('On the dl', data)
-      st.checkbox('Check me out')
-      st.radio('Radio', [1,2,3])
-      st.selectbox('Select', [1,2,3])
-      st.multiselect('Multiselect', [1,2,3])
-    else:
-      col1, col2, col3 = st.columns((0.2,1,0.3))
-        with col1:
-          with st.expander("Fonte:"):
-          st.write("IBGE, [2010].")
-        with col2:
-          st.slider('Slide me', min_value=0, max_value=10)
-          st.select_slider('Slide to select', options=[1,'2'])
-          st.text_input('Enter some text')
-          st.number_input('Enter a number')
-          st.text_area('Area for textual entry')
-        with col3:
-          st.date_input('Date input')
-          st.time_input('Time entry')
-          st.file_uploader('File uploader')
-          st.color_picker('Pick a color')
+  elif categoria == "Categoria 2":
+    st.button('Hit me')
+    #st.download_button('On the dl', data)
+    st.checkbox('Check me out')
+    st.radio('Radio', [1,2,3])
+    st.selectbox('Select', [1,2,3])
+    st.multiselect('Multiselect', [1,2,3])
+  else:
+    col1, col2, col3 = st.columns((0.2,1,0.3))
+      with col1:
+        with st.expander("Fonte:"):
+        st.write("IBGE, [2010].")
+      with col2:
+        st.slider('Slide me', min_value=0, max_value=10)
+        st.select_slider('Slide to select', options=[1,'2'])
+        st.text_input('Enter some text')
+        st.number_input('Enter a number')
+        st.text_area('Area for textual entry')
+      with col3:
+        st.date_input('Date input')
+        st.time_input('Time entry')
+        st.file_uploader('File uploader')
+        st.color_picker('Pick a color')
