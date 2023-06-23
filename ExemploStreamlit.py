df2a8fe56663d9b7ba84b2286b1ac265e5fd5b03
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-06-23 05:49:02 -0300

    Update ExemploStreamlit.py

--- ExemploStreamlit.py
+++ ExemploStreamlit.py
@@ -5,20 +5,78 @@ import streamlit as st
 name = "Mapa da Desigualdade"
 st.set_page_config(layout="wide", page_title=name)
 
-import streamlit as st
-
-primaryColor="#F63366"
-backgroundColor="#FFFFFF"
-secondaryBackgroundColor="#F0F2F6"
-textColor="#262730"
-font="Ysabeau Office"
-st.title("Meu Aplicativo Streamlit")
-st.write("Este é um exemplo de como usar uma fonte personalizada no Streamlit.")
-
-
 #Opções do menu
 st.sidebar.markdown('# Mapa da Desigualdade')
 options = st.sidebar.selectbox("Selecione uma opção:", ("Paraná", "Núcleo Territorial Central"))
+
+st.title("Mapa da Desigualdade: Contextualização")
+st.title("População")
+
+#-----------------Parte do mapa
+
+#Arquivos
+PR = 'https://raw.githubusercontent.com/jhbricks/mapades/main/dados/geojson/PR.geojson'
+NTC = 'https://raw.githubusercontent.com/jhbricks/mapades/main/dados/geojson/NTC.geojson'
+contx = 'https://raw.githubusercontent.com/jhbricks/mapades/main/dados/csv/contexto.csv'
+pop = 'https://raw.githubusercontent.com/jhbricks/mapades/main/dados/csv/pop_2021.csv'
+
+#Unir csv com geojson
+import pandas as pd
+import geopandas as gpd
+
+geojson_pr = gpd.read_file(PR)
+
+csv_pop = pd.read_csv(pop)
+pr_pop = geojson_pr.merge(csv_pop, on="Município")
+csv_contx = pd.read_csv(contx)
+pr_contx = geojson_pr.merge(csv_contx, on="Município")
+
+#Mapa da População
+import leafmap
+# Configura o mapa geral
+m = leafmap.Map(center=[-51.8, -24.7],
+                zoom= 11,
+                draw_control=False,
+                measure_control=False,
+                fullscreen_control=False,
+                attribution_control=True
+                               )
+m.add_data(
+    pr_pop,
+    column='População',
+    scheme='FisherJenks',
+    k = 7,
+    cmap= 'YlGnBu',
+    fields= ['Município','População'],
+    legend_title='População total',
+    legend_position = "bottomright",
+    style={"stroke": True, "color": "#000000", "weight": 1, "fillOpacity": 1}
+)
+m
+col1, col2 = st.columns(2)
+#Coluna 1: 
+    with col1:
+      st.header('Maior e Menor Valor')
+      import pandas as pd
+      maior_populacao = dataframe['População'].max()
+      menor_populacao = dataframe['População'].min()
+      municipio_maior_populacao = dataframe.loc[dataframe['População'] == maior_populacao, 'Municípios'].iloc[0]
+      municipio_menor_populacao = dataframe.loc[dataframe['População'] == menor_populacao, 'Municípios'].iloc[0]
+      print(f"Maior: ({municipio_maior_populacao}) com {maior_populacao} habitantes")
+      print(f"Menor população: {menor_populacao} ({municipio_menor_populacao})")
+    with col2:
+      st.header('Observações:')
+      st.subheader('ANO BASE: 2021')
+      st.subheader('FÓRMULA: População total do município')
+      st.subheader('FONTE: IBGE, 2021')
+      st.subheader('OBSERVAÇÕES: População total de acordo com o censo do IBGE de 2021')
+            
+        
+
+
+
+
+
 
 if options == "Paraná":
   st.subheader("Mapa da Desigualdade do Paraná")
