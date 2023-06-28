b9e6ee148d778acabe236933494a6bafbd846379
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-06-28 03:09:53 -0300

    Update ExemploStreamlit.py

--- ExemploStreamlit.py
+++ ExemploStreamlit.py
@@ -74,8 +74,11 @@ if options == "Paraná":
       m.to_streamlit()
       col1, col2 = st.columns(2)
       with col1:
-        #st.subheader(':green[Maior] :black[e] :red[menor] :black[valor do indicador:]')
-        st.markdown(':green[Maior] :black[e] :red[menor] :black[valor do indicador:]')
+        st.subheader(':green[Maior] :black[e] :red[menor] :black[valor do indicador:]')
+        #st.markdown(':green[Maior] :black[e] :red[menor] :black[valor do indicador:]')
+        st.markdown("<h3><font size='5' color='red'>Menor</font> <font size='3' color='black'>e</font> <font size='5' color='green'>maior</font> <font size='3' color='black'>valor do indicador</font></h3>",
+                    unsafe_allow_html=True)
+
         arrow_d = '\U0001F82B'  # Ícone de seta para baixo
         arrow_u = '\U0001F829'  # Ícone de seta para cima
         min_str = f"{min_municipio}"
