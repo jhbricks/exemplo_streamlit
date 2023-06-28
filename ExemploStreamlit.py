defa99c9960c2b602c47070bcf0ddce147565946
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-06-28 02:06:21 -0300

    Update ExemploStreamlit.py

--- ExemploStreamlit.py
+++ ExemploStreamlit.py
@@ -77,7 +77,7 @@ if options == "Paraná":
         arrow_icon = '\u2193'
         output_str = f"{min_municipio}"
         st.text(output_str)
-        st.header(':green['\u2193']') st.subheader(output_str) 
+        st.header(':green[arrow_icon]') st.subheader(output_str) 
       with col2:
         st.button('Hit me')
         
